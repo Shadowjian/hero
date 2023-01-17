aea7f3440f4aeb132f9b72f543743e46fc674c85
poldong1001 <poldong2019@gmail.com>
2023-01-17 22:21:49 +0800

    updated

--- src/layout/pages/Home.jsx
+++ src/layout/pages/Home.jsx
@@ -1,8 +1,237 @@
 import React from 'react'
+import SecTwo from '../pages/home/SecTwo'
+import SecOne from '../pages/home/SecOne'
+import PopularServices from '../../layout/pages/home/PopularServices'
+import Container from '@mui/material/Container'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel';
+import { Box, createTheme,styled, CardMedia, TextField,Button,InputBase,Hidden, Typography, Drawer } from '@mui/material'
+import Logo from '../../assets/images/Logo.png'
+import workerImage from '../../assets/images/workerImage.png'
+import webbg from  '../../assets/images/webbg.png'
+ 
+import zIndex from '@mui/material/styles/zIndex'
 
-export default function Home() {
+ 
 
+const BgImage = styled(CardMedia)({
+    height:'70%',
+    width:'100%',
+    position:'absolute',
+    zIndex:'-1',
+    margin:'0'
+    
+})
+
+const WebLogo =styled(CardMedia)({
+    height:'250px',
+    width:'250px'
+})
+
+const PopularBtn = styled(Button)({
+    backgroundColor:'#494747',
+     fontSize:'9px',
+     padding:'10px',
+    marginBottom:'30px',
+    color:'whitesmoke',
+    display:'flex'
+
+})
+ 
+
+function Home() {
   return (
-    <div>Home</div>
+    <>
+   
+    <BgImage
+        sx={{
+            margin:'0',
+            width:{xs:'100%'},
+            height:{xs:'75%',sm:'90%',md:'70%'}
+        }}
+        image={webbg}
+        />
+        <Container 
+        sx={{
+            margin:'0',
+            paddingLeft:{xs:'0'},
+            paddingRight:{xs:'0'},
+            display:'flex',
+            justifyContent:{xs:'center',sm:'center',md:'center',lg:'left'},
+            flexDirection:{xs:'column'}
+        }}
+        maxWidth="lg"
+        >
+        <Box
+        display={'flex'}
+        flexDirection={'column'}
+        >
+            <Hidden
+            only={['xs','sm','md']}
+            >
+                <WebLogo
+                sx={{
+                    mt:'40px',
+                }}
+                image={Logo}
+                />
+            </Hidden>
+                
+                <Box
+                    display={'flex'}
+                    sx={{
+                        justifyContent:{xs:'center'},
+                        flexDirection:{xs:'row'},
+                        mt:{xs:'20px'}
+                    }}
+                    >
+                        <Hidden
+                        only={['xs']}
+                        >
+                        <TextField
+                            variant='filled'
+                            label={'SEARCH HERO'}
+                        sx={{
+                            background: 'rgba(62, 56, 56, 0.40)',
+                            backdrop:'blur(2.7px)',
+                            WebkitBackdropFilter:'blur(3.7px)',
+                            height:{xs:'50px'},
+                            width:{xs:'200px',sm:'500px',md:'700px',lg:'400px'},
+                            }}
+                            />
+                            <Button
+                            sx={{
+                                bgcolor:'red',
+                                height:'55px',
+                                color:'whitesmoke'
+                                }}
+                                >
+                                SEARCH
+                            </Button>
+                        </Hidden>
+                    
+                </Box>
+                            <Box
+                            mt={'20px'}
+                            display={'flex'}
+                            flexDirection={'column'}
+                            
+                            sx={{
+                                justifyContent:'center',
+                                alignItems:{xs:'center'}
+                            }}
+                            >
+                                
+                                <Box
+                                height={'30px'}
+                                display={'flex'}
+                                gap={1}
+                                // bgcolor={'red'}
+                               
+                                >
+                                    <Carousel 
+                                        sx={{
+                                            
+                                        }}
+                                        width={'300px'}
+                                        display={'flex'}
+                                        
+                                        
+                                        infiniteLoop autoPlay
+                                    >
+                                    <Box
+                                    display={'flex'}
+                                    gap={2}
+                                    >
+                                    <PopularBtn>
+                                        MANICURE
+                                    </PopularBtn>
+                                    <PopularBtn>
+                                        CARPENTER
+                                    </PopularBtn>
+                                    <PopularBtn>
+                                        PLUMBER
+                                    </PopularBtn>
+                                    <PopularBtn>
+                                        ELECTRICIAN
+                                    </PopularBtn>
+                                    </Box>
+                                    <Box
+                                    display={'flex'}
+                                    gap={2}
+                                    >
+                                    <PopularBtn>
+                                        BAKER
+                                    </PopularBtn>
+                                    <PopularBtn>
+                                        SINGER
+                                    </PopularBtn>
+                                    <PopularBtn>
+                                        VOLCANIZE
+                                    </PopularBtn>
+                                    <PopularBtn>
+                                        YAYA
+                                    </PopularBtn>
+                                    </Box>
+                                    </Carousel>
+                                </Box>
+                              
+                            </Box>
+        </Box>
+        <Box
+        
+        display={'flex'}
+        alignItems={'center'}
+        sx={{
+            mt:{xs:'54px'},
+            flexDirection:{xs:'column'}
+        }}
+         
+            // bgcolor={'blue'}
+        >
+            <Typography
+            
+            sx={{
+                lineHeight:'1',
+                fontSize:'30px',
+                color:'red',
+                fontWeight:{xs:'bold'},
+                fontSize:{xs:'25px',sm:'50px'}
+            }}
+            >
+                YOUR PROBLEM
+            </Typography>
+            <Typography
+            
+            sx={{
+                lineHeight:'1',
+                color:'#494747',
+                fontWeight:{xs:'bold'},
+                fontSize:{xs:'45px',sm:'70px'}
+            }}
+            >
+                 WE SOLVE
+            </Typography>
+            <CardMedia 
+             sx={{
+                width:{xs:'90%'},
+                height:{xs:'auto'}
+             }}
+            component={'img'}
+            
+            image={workerImage} 
+            />        
+        </Box>
+        <PopularServices/>
+        <SecOne/>
+        <SecTwo/> 
+             {/* <PopularServices/> */}
+           
+          
+        </Container>
+        
+    </>
   )
 }
+
+export default Home
---
+++ src/layout/pages/home/FooterSec.jsx
@@ -0,0 +1,203 @@
+import React, { useState } from 'react'
+import Typography from '@mui/material/Typography'
+import {Facebook, Instagram,Twitter,LinkedIn} from '@mui/icons-material'
+import { Box, styled,ListItemButton, ListItemIcon, ListItemText, List, } from '@mui/material'
+import StarBorder from '@mui/icons-material/StarBorder';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import { padding, textAlign } from '@mui/system';
+
+const FooterList = styled(Typography)({
+    fontSize:'13px',
+    textAlign:'left',
+    paddingLeft:'15px'
+})
+
+// const Collapse = styled=(Typography)({
+//     fontSize:'10px',
+//     color:'grey'
+// })
+ 
+function FooterSec() {
+
+    const[hero,setHero] = useState(false);
+    
+    const handleClick = () => {
+        setHero(!hero);
+    }
+ 
+    const[about,setAbout] = useState(false);
+    
+    const handleClickTwo = () => {
+        setAbout(!about);
+    }
+
+    const[help,setHelp] = useState(false);
+    
+    const handleClickThree = () => {
+        setHelp(!help);
+    }
+
+  return (
+    <>
+    <ListItemButton
+     sx={{
+        width:{xs:'100%'},
+        bgcolor:{xs:'whitesmoke'}
+    }}
+    onClick={handleClick}
+    >
+        
+        <ListItemText>
+           <Typography
+           sx={{
+            
+           }}
+           >
+             HERO
+        </Typography>
+        </ListItemText>
+        {hero ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={hero} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+            <FooterList>
+            YOUR PROBLEM WE SOLVE
+            </FooterList>
+             <FooterList>
+             Copyright &copy; 2022 HERO
+             </FooterList>
+             
+        </List>
+      </Collapse>
+
+      <ListItemButton
+      sx={{
+        width:{xs:'100%'}
+      }}
+      onClick={handleClickTwo}>
+        
+        <ListItemText sx={{fontWeight:'bold'}} primary="GET HELP" />
+        {about ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={about} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+            <FooterList>
+            HOW IT WORKS
+            </FooterList>
+            <FooterList>
+            FREQUENTLY QUESTIONS
+            </FooterList>
+            <FooterList>
+            PRIVACY POLICY
+            </FooterList>
+            <FooterList>
+            TERMS AND CONDITIONS
+            </FooterList>
+             
+        </List>
+      </Collapse>
+
+      <ListItemButton sx={{
+        width:{xs:'100%'},
+        bgcolor:{xs:'whitesmoke'}
+      }} onClick={handleClickThree}>
+        
+        <ListItemText sx={{fontWeight:'bold'}} primary="ABOUT US" />
+        {help ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={help} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+            <FooterList>
+            ABOUT US
+            </FooterList>
+            <FooterList>
+            OUR SERVICE
+            </FooterList>
+            <FooterList>
+            TESTIMONIALS
+            </FooterList>
+            <FooterList>
+            JOIN US NOW
+            </FooterList>
+            
+        </List>
+      </Collapse>
+
+     
+      
+    <Box
+    sx={{
+        bgcolor:{xs:'#494747'},
+        height:{xs:'auto'},
+        width:{xs:'100%'},
+        
+    }}
+    >
+        <Typography
+        sx={{
+            fontSize:{xs:'15px'},
+             color:{xs:'whitesmoke'},
+             display:{xs:'flex'},
+             flexDirection:{xs:'column'},
+             alignItems:{xs:'center'},
+             justifyContent:{xs:'space-around'},
+             padding:{xs:'20px'}
+        }}
+        >
+            CONTACT US
+        </Typography>
+        <Box
+        sx={{
+            display:{xs:'flex'},
+            justifyContent:{xs:'center'},
+            gap:{xs:'20px'}
+        }}
+        >
+            <Facebook
+            sx={{
+                color:'whitesmoke'
+            }}
+            fontSize='large'
+            />
+            <Twitter
+            sx={{
+                color:'whitesmoke'
+            }}
+            fontSize='large'
+            />
+            <Instagram
+            sx={{
+                color:'whitesmoke'
+            }}
+            fontSize='large'
+            />
+            <LinkedIn
+            sx={{
+                color:'whitesmoke'
+            }}
+            fontSize='large'
+            />
+        </Box>
+        <Box
+        sx={{
+            padding:{xs:'20px',sm:'30px'},
+            textAlign:{xs:'center'}
+        }}
+        >
+            <Typography
+            color='whitesmoke'
+            
+            >
+                support@HERO.com <br />
+                63 (945) 773 0024
+            </Typography>
+        </Box>
+    </Box>
+    </>
+  )
+}
+
+export default FooterSec
