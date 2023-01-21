ad18c3fcfeb2da20191813ac0cdb602b3fb5bbc5
Shadowjian <karakalel.adorable@gmail.com>
2023-01-22 02:05:35 +0800

    updating quotes

--- src/states/defaultStates.jsx
+++ src/states/defaultStates.jsx
@@ -21,49 +21,49 @@ export const gigsDefaultState = [
   {
     id: 1,
     hero: "hajji",
-    desc: "knows how to nail",
+    desc: "Every moment I shape my destiny with a chisel - I am the carpenter of my own soul.",
     power: "carpenter"
   },
   {
     id: 2,
     hero: "polds",
-    desc: "can stir you to heaven",
-    power: "driver"
+    desc: "Leave sooner, drive slower, live longer.",
+    power: "Driver"
   },
   {
     id: 3,
     hero: "joseph",
-    desc: "will take care of your wet",
+    desc: "I repair what your husband ﬁxed",
     power: "plumber"
   },
   {
     id: 4,
     hero: "zen",
-    desc: "aahhahahaha",
-    power: "pornstar"
+    desc: "Sweet lover",
+    power: "driver"
   },
   {
     id: 5,
     hero: "felix",
-    desc: "your pain my pleasure",
+    desc: "There are some things in life where it's better to receive than to give, and massage is one of them.",
     power: "masseur"
   },
   {
     id: 6,
     hero: "nicky",
-    desc: "aahhahahaha",
+    desc: "You can get a good doctor in a minute and a half. Getting a really good electrician - that's hard.",
     power: "electrician"
   },
   {
     id: 7,
     hero: "winfred",
-    desc: "aahhahahaha",
+    desc: "Take care of your car in the garage, and the car will take care of you on the road.",
     power: "mechanic"
   },
   {
     id: 8,
     hero: "sir felix",
-    desc: "aahhahahaha",
+    desc: "A weed is but an unloved flower",
     power: "gardener"
   }
 ]
